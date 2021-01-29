1475a8414a693a510a6e05809c0234400feaaefb
Gurvir Singh Tarlok Singh Bhogal <40941950+GurvirSingh@users.noreply.github.com>
2021-01-29 16:24:59 -0600

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <div className="ui secondary pointing menu" id="yt--header">
             <Link href="/" className="item"><i class="video icon"></i>Live Streamer</Link>
             <Link href="/list" className="item">Wiki Search</Link>
-            <Link href="/dropdown" className="item">Dropdown</Link>
+            <Link href="/dropdown" className="item">Live Stream</Link>
 
             <div className="right menu">
                     {/* <Link to='/' className="item">
